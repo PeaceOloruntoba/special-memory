e0a54c76a7bebf0350a161e4c962b73cb2d1fbdd
PeaceOloruntoba <profprincepeace@gmail.com>
2025-08-13 01:59:07 +0100

    move

--- src/pages/designer/Clients.tsx
+++ src/pages/designer/Clients.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export default function Clients() {
   return (
     <div>Clients</div>
--- src/pages/designer/clients/ClientDetails.tsx
+++
@@ -1,5 +0,0 @@
-export default function ClientDetails() {
-  return (
-    <div>ClientDetails</div>
-  )
-}
