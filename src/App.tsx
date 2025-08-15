a1cee46fa8fa2baed84c542f017011419b90d925
PeaceOloruntoba <profprincepeace@gmail.com>
2025-08-15 20:38:12 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ export default function App() {
             }
           />
           <Route
-            path="/pattern-design"
+            path="/patterns"
             element={
               <AdminGuard>
                 <Default pageTitle="Pattern Design" />
